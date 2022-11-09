853432250c0ba27efa36315ceae7d09f8438a4c5
Ritik22-tech <2003ayriritik@gmail.com>
2022-11-09 19:10:50 +0530

    Initial Commit for Newsapp

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,8 +2,7 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 export class Navbar extends Component {
-    // static propTypes = {}
-
+    
     render() {
         return (<>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
